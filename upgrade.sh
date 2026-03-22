dd85e081fe1051029ece35971dd08973e46afcda
Nicklas Boman <smurfd@gmail.com>
2026-03-22 14:42:08 +0100

    Update upgrade.sh

--- upgrade.sh
+++ upgrade.sh
@@ -1,13 +1,15 @@
 #!/bin/sh
 # Use with care, have not tried yet on a live system, only vm so far
-# TODO: is the installboot working?
 version=10.0
 arch=amd64
-doas pkgin install -y wget
+today=`date date +"%y-%m-%d"`
+echo Downloading sets...
+doas pkgin -y install wget
 wget -r --no-parent --no-check-certificate http://ftp.fr.netbsd.org/pub/NetBSD/NetBSD-$version/$arch/binary/sets/ > /dev/null 2>&1
 cd ftp.fr.netbsd.org/pub/NetBSD/NetBSD-$version/$arch/binary/sets/
 
-doas mv /netbsd /netbsd.old
+doas mv /netbsd /netbsd.$today
+doas mv /boot /boot.$today
 
 doas tar Jxf kern-GENERIC.tar.xz -C /
 doas tar Jxf base.tar.xz -C /
@@ -26,7 +28,9 @@ doas tar Jxf xserver.tar.xz -C /
 echo skipping: debug.tar.xz etc.tar.xz games.tar.xz xdebug.tar.xz xetc.tar.xz
 echo Extraction complete
 doas installboot -v /dev/rdk0 /usr/mdec/bootxx_ffsv2
+doas cp /usr/mdec/boot /boot
 
 echo "http://cdn.NetBSD.org/pub/pkgsrc/packages/NetBSD/$arch/$version/All" > repo.conf
+doas cp /usr/pkg/etc/pkgin/repositories.conf /usr/pkg/etc/pkgin/repositories.conf.$today
 doas cp repo.conf /usr/pkg/etc/pkgin/repositories.conf
 doas pkgin update && doas pkgin upgrade
